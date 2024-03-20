95925bacce759f855df54aa9ad664faa3c32d673
Ryazansev Vladimir <elf.forest@yandex.ru>
2024-03-21 01:13:21 +0300

    Чт 21 мар 2024 01:13:21 MSK

--- dotFile/apt.bash
+++ dotFile/apt.bash
@@ -5,15 +5,15 @@ if [[ -f /bin/dialog ]]; then
   sudo apt install -y dialog
 fi
 
-array="mc sbcl git curl spell aspell aspell-ru graphviz openjdk-19-jre-headless hunspell hunspell-ru buku zsh clangd cmake build-essential tmux maven librust-libudev-sys-dev fonts-firacode fonts-cantarell"
+array="mc sbcl git curl spell aspell aspell-ru graphviz openjdk-19-jre-headless hunspell hunspell-ru buku zsh clangd cmake build-essential tmux maven librust-libudev-sys-dev fonts-firacode fonts-cantarell git-annex python3-pip bspwm"
 
 echo "$array"
-# for item in ${array[*]}
-# do
-#    sudo apt install -y $item
-# done
+for item in ${array[*]}
+do
+   sudo apt install -y $item
+done
 
-# echo -e "система готова программы устоновленны"
+echo -e "система готова программы устоновленны"
 
 # zsh по умолчанию
 
