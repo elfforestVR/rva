344c1ea012ad991eed87ff09edb142e3aa063f12
Ryazansev W <elf.forest@yandex.ru>
2024-03-28 14:23:41 +0300

    Чт 28 мар 2024 14:23:41 MSK

--- home/Blog/gulpfile.js
+++ home/Blog/gulpfile.js
@@ -4,7 +4,7 @@ const {src, dest } = require("gulp");
 const gulp = require ("gulp");
 const browserSync = require ("browser-sync");
 const del = require ("del");
-const autoprefixer = require ("gulp-autoprefixer");
+// const autoprefixer = require ("gulp-autoprefixer");
 const cssbeautify = require ("gulp-cssbeautify");
 const cssnano = require ("gulp-cssnano");
 const imagemin = require ("gulp-imagemin");
@@ -12,9 +12,15 @@ const plumber = require ("gulp-plumber");
 const rename = require ("gulp-rename");
 const rigger = require ("gulp-rigger");
 const sass = require ("gulp-sass")(require ('sass'));
-const removeComments = require ("gulp-strip-css-comments");
+// const removeComments = require ("gulp-strip-css-comments");
 const uglify = require ("gulp-uglify");
 const panini = require ("panini");
 
 
 
+const path = {
+	build: {
+		html: "dist/",
+		cd: "dist/assets/css"
+	}
+}
