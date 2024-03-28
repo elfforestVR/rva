503e6cf53ed260125aa5a66f13958c44dd30fb3f
Ryazansev Vladimir <elf.forest@yandex.ru>
2024-03-28 06:55:39 +0300

    Чт 28 мар 2024 06:55:39 MSK

--- home/Templates/gulp-template-2024/gulpfile.js
+++ home/Templates/gulp-template-2024/gulpfile.js
@@ -1,6 +1,20 @@
 "use strict"
 
-const {src, dest } = require("gulp")
-const gulp = require ("gulp")
+const {src, dest } = require("gulp");
+const gulp = require ("gulp");
+const browserSync = require ("browser-sync");
+const del = require ("del");
+const autoprefixer = require ("gulp-autoprefixer");
+const cssbeautify = require ("gulp-cssbeautify");
+const cssnano = require ("gulp-cssnano");
+const imagemin = require ("gulp-imagemin");
+const plumber = require ("gulp-plumber");
+const rename = require ("gulp-rename");
+const rigger = require ("gulp-rigger");
+const sass = require ("gulp-sass")(require ('sass'));
+const removeComments = require ("gulp-strip-css-comments");
+const uglify = require ("gulp-uglify");
+const panini = require ("panini");
+
 
 
